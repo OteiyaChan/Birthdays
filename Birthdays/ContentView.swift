8acce940db431698e561b31952aae1811a1a5253
Oteiya Chan <oteiyachan@gmail.com>
2025-07-25 11:34:16 -0700

    Added delete function

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -18,40 +18,54 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            List(friends) { friend in
-                HStack {
-                    Text(friend.name)
-                    Spacer()
-                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
-                }//hstack
-           }//friend in
-            .navigationTitle("Birthdays")
-            .safeAreaInset(edge: .bottom) {
-                VStack(alignment: .center, spacing: 20) {
-                    Text("New Birthday")
-                        .font(.headline)
-                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
-                        TextField("Name", text: $newName)
-                            .textFieldStyle(.roundedBorder)
-                    }
-                    Button("Save") {
-                        let newFriend = Friend(name: newName, birthday: newBirthday)
-                        //friends.append(newFriend)
-                        context.insert(newFriend)
-                        newName = ""
-                        newBirthday = .now
-                    }
-                    .bold()
+            List {
+                ForEach(friends) { friend in
+                    HStack {
+                        HStack {
+                            Text(friend.name)
+                            Spacer()
+                            Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                        }
+                    }//hstack
                 }
-                .padding()
-                .background(.bar)
+                    .onDelete(perform: deleteFriend)
+                }//friend in
+                .navigationTitle("Birthdays")
+                .safeAreaInset(edge: .bottom) {
+                    VStack(alignment: .center, spacing: 20) {
+                        Text("New Birthday")
+                            .font(.headline)
+                        DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                            TextField("Name", text: $newName)
+                                .textFieldStyle(.roundedBorder)
+                        }
+                        Button("Save") {
+                            let newFriend = Friend(name: newName, birthday: newBirthday)
+                            //friends.append(newFriend)
+                            context.insert(newFriend)
+                            newName = ""
+                            newBirthday = .now
+                        }
+                        .bold()
+                    }
+                    .padding()
+                    .background(.bar)
+                }
             }
+        
+    }//body
+        
+        func deleteFriend(at offsets: IndexSet) {
+            for index in offsets {
+                let friendToDelete = friends[index]
+                context.delete(friendToDelete)
             }
         }
-    }
-
-
+}//struct
+    
+    
 #Preview {
     ContentView()
-        .modelContainer(for: Friend.self, inMemory: true)
-}
+            .modelContainer(for: Friend.self, inMemory: true)
+    }
+
